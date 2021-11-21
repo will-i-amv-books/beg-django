ed2212d5d7ef7237fff6363e75af37f113a82c91
William <wsebasvb@gmail.com>
2021-11-21 16:51:46 -0300

    Install and track the docutils package, then use it to enable the Django documentation app

--- coffeehouse_app/coffeehouse/urls.py
+++ coffeehouse_app/coffeehouse/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from coffeehouse.about import views as about_views
 
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$',TemplateView.as_view(template_name='homepage.html')),
     url(r'^about/', about_views.contact),
+    url(r'^$',TemplateView.as_view(template_name='homepage.html')),
 ]
