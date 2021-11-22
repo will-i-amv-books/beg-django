f9f5ee5dfdd4c40b0dfd83f169b10b2dbdbffa80
William <wsebasvb@gmail.com>
2021-11-22 18:22:43 -0300

    Add also the new parameter in the URL definition in the view method

--- coffeehouse_app/coffeehouse/stores/views.py
+++ coffeehouse_app/coffeehouse/stores/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-def detail(request, store_id='1'):
+def detail(request, store_id='1', location=None):
     # Access store_id with 'store_id' variable
     return render(request, 'stores/detail.html')
