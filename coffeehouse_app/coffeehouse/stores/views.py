bd920c91f3fc670a55b8a360a51f89ff5c3479c9
William <wsebasvb@gmail.com>
2021-11-22 18:25:31 -0300

    Modify the view method to capture the URL query strings

--- coffeehouse_app/coffeehouse/stores/views.py
+++ coffeehouse_app/coffeehouse/stores/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def detail(request, store_id='1', location=None):
-    # Access store_id with 'store_id' variable
+    # Extract 'hours' or 'map' value appended to url as ?hours=sunday&map=flash
+    hours = request.GET.get('hours', '') # 'hours' has value 'sunday' or '' if hours not in url
+    map = request.GET.get('map', '') # 'map' has value 'flash' or '' if map not in url
     return render(request, 'stores/detail.html')
