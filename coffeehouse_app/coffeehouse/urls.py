88dd87aeacf86a51cd74570eb928d217aea21f1e
William <wsebasvb@gmail.com>
2021-11-22 18:21:46 -0300

    Add extra parameter in URL definition

--- coffeehouse_app/coffeehouse/urls.py
+++ coffeehouse_app/coffeehouse/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
         {'onsale':True},
     ),
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
-    url(r'^stores/',stores_views.detail),
+    url(r'^stores/', stores_views.detail, {'location':'headquarters'}),
     url(r'^stores/(?P<store_id>\d+)/', stores_views.detail),
 ]
