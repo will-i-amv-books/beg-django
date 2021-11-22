bda961f6dda2ee04d902bbbbb12aa95734141744
William <wsebasvb@gmail.com>
2021-11-22 17:44:51 -0300

    Add url to match the homepage of the about section

--- coffeehouse_app/coffeehouse/urls.py
+++ coffeehouse_app/coffeehouse/urls.py
@@ -22,6 +22,7 @@ from coffeehouse.about import views as about_views
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^about/index/',TemplateView.as_view(template_name='index.html')),
     url(r'^about/', about_views.contact),
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
 ]
