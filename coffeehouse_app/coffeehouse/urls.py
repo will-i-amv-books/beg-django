57d5e1e2af09b7e4bcebd5731004af5a53aa36e1
William <wsebasvb@gmail.com>
2021-11-23 08:03:33 -0300

    Add url namespaces for distinct banner app instances

--- coffeehouse_app/coffeehouse/urls.py
+++ coffeehouse_app/coffeehouse/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
     url(r'^admin/$', admin.site.urls),
     url(r'^about/', include('coffeehouse.about.urls', namespace="about")), 
     url(r'^stores/', include('coffeehouse.stores.urls', namespace="stores"), {'location':'headquarters'}), 
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage"), 
+    url(r'^coffeebanners/', include('coffeehouse.banners.urls', namespace="coffee-banners")), 
+    url(r'^teabanners/', include('coffeehouse.banners.urls', namespace="tea-banners")), 
+    url(r'^foodbanners/', include('coffeehouse.banners.urls', namespace="food-banners")), 
     url(
         r'^drinks/(?P<drink_name>\D+)/', 
         TemplateView.as_view(template_name='drinks/index.html'),
