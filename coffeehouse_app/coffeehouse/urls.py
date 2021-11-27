54718f75a345a539aea2241b90ae87a2e79fbd05
William <wsebasvb@gmail.com>
2021-11-27 10:21:10 -0300

    Changes * Add URL addresses of custom error templates * Add app names for banner-related URLs * Reorder all urls so that granular URLs are first   and broad URLs are last

--- coffeehouse_app/coffeehouse/urls.py
+++ coffeehouse_app/coffeehouse/urls.py
@@ -18,20 +18,23 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 
+# Overrides the default 400 handler django.views.defaults.bad_request
+handler400 = 'coffeehouse.utils.views.bad_request'
+# Overrides the default 403 handler django.views.defaults.permission_denied
+handler403 = 'coffeehouse.utils.views.permission_denied'
+# Overrides the default 404 handler django.views.defaults.page_not_found
+handler404 = 'coffeehouse.utils.views.page_not_found'
+# Overrides the default 500 handler django.views.defaults.server_error
+handler500 = 'coffeehouse.utils.views.server_error'
+    
 urlpatterns = [
-    url(r'^admin/doc/$', include('django.contrib.admindocs.urls')),
-    url(r'^admin/$', admin.site.urls),
+    url(r'^admin/doc/',  include('django.contrib.admindocs.urls')), 
+    url(r'^admin/',  admin.site.urls), 
     url(r'^about/', include('coffeehouse.about.urls', namespace="about")), 
-    url(r'^stores/', include('coffeehouse.stores.urls', namespace="stores"), {'location':'headquarters'}), 
+    url(r'^stores/', include('coffeehouse.stores.urls', namespace="stores")), 
+    url(r'^coffeebanners/', include('coffeehouse.banners.urls', namespace="coffee-banners", app_name="banners_adverts")), 
+    url(r'^teabanners/', include('coffeehouse.banners.urls', namespace="tea-banners", app_name="banners_adverts")), 
+    url(r'^foodbanners/', include('coffeehouse.banners.urls', namespace="food-banners", app_name="banners_adverts")), 
+    url(r'^drinks/(?P<drink_type>\D+)/', TemplateView.as_view(template_name='drinks/index.html'), {'onsale':True}, name="drink_type"), 
     url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage"), 
-    url(r'^coffeebanners/', include('coffeehouse.banners.urls', namespace="coffee-banners")), 
-    url(r'^teabanners/', include('coffeehouse.banners.urls', namespace="tea-banners")), 
-    url(r'^foodbanners/', include('coffeehouse.banners.urls', namespace="food-banners")), 
-    url(
-        r'^drinks/(?P<drink_name>\D+)/', 
-        TemplateView.as_view(template_name='drinks/index.html'),
-        {'onsale':True},
-        name="drink",
-    ),
-    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage")
 ]
