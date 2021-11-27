5af9d2aee2e6db00f1d9bab6dbea975a5b149c43
William <wsebasvb@gmail.com>
2021-11-27 10:10:40 -0300

    Move urls.py to correct app path

--- coffeehouse_app/coffeehouse/about/urls.py
+++ coffeehouse_app/coffeehouse/about/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^contact/$', views.contact, name="contact"), 
     url(r'^$', views.index, name="index"), 
+    url(r'^(?P<store_id>\d+)/$', views.index, name="index_withid"), 
+    url(r'^contact/$', views.ContactPage.as_view(), name="contact"), 
+    url(r'^contact/(?P<store_id>\d+)/$', views.ContactPage.as_view(), name="contact_withid"), 
 ]
