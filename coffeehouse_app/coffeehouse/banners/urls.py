f8b3637d59a4bf7f85c7b09de43b4bc9b9433e62
William <wsebasvb@gmail.com>
2021-11-27 10:28:39 -0300

    Codify all files as UTF-8

--- coffeehouse_app/coffeehouse/banners/urls.py
+++ coffeehouse_app/coffeehouse/banners/urls.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.conf.urls import url
-from . import views
+from . import views 
 
 
 app_name = 'banners_adverts'
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.index , name="index"),
 ]
-
--- coffeehouse_app/coffeehouse/banners/views.py
+++ coffeehouse_app/coffeehouse/banners/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
+from django.core.urlresolvers import resolve
 
-# Create your views here.
+
+def index(request):    
+    return render(request, 'banners/index.html')
