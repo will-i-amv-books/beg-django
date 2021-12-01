98acac7879436816abfe82d16f303c83e5e7d311
William <wsebasvb@gmail.com>
2021-12-01 07:40:43 -0300

    Add view methods for the index and detail templates

--- coffeehouse_app/coffeehouse/drinks/views.py
+++ coffeehouse_app/coffeehouse/drinks/views.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    drink_list = ['mocha', 'espresso', 'latte']
+    return render(request, 'drinks/index.html', {'drinks':drink_list})
+
+
+def detail(request, drink_type):
+    if drink_type == "mocha":
+        description = "Our superior dark coffee made from beans from Arabia"
+    elif drink_type == "espresso":
+        description = "Our strong coffee brewed by forcing steam under pressure \
+            through darkly roasted,  powdered coffee beans"
+    else: 
+        description = "Our coffee made with hot milk"
+    return render(
+        request,
+        'drinks/detail.html', 
+        {'drink':drink_type, 'drink_description':description}
+    )
