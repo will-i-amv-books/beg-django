c658ca98b12bf38664057677e0c5a52c584d9abc
William <wsebasvb@gmail.com>
2022-02-14 18:01:30 -0300

    In forms.py: * Add new forms, don't initialize them
    In views.py:
    * Modify body of 'index' and 'detail'

--- coffeehouse_app/coffeehouse/drinks/urls.py
+++ coffeehouse_app/coffeehouse/drinks/urls.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.conf.urls import include,  url
 from coffeehouse.drinks import views as drinks_views
-
 
 urlpatterns = [
     url(r'^(?P<drink_type>\D+)/$', drinks_views.detail, name="detail"), 
